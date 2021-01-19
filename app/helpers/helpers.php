8bfa036c2192435cd4dfca8ee83cd5365c877b15
Francisco <frangarmu95@gmail.com>
2021-01-18 21:59:43 -0600

    habilitar helpers error corregido

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -1,27 +1,27 @@
 <?php
 
-if (!functon_exists('view')) {
+if (!function_exists('view')) {
   function view($view)
   {
     return new App\Core\Response($view, 'html');
   }
 }
 
-if (!functon_exists('view')) {
+if (!function_exists('view')) {
   function json($data)
   {
     return new App\Core\Response('', 'json', $data);
   }
 }
 
-if (!functon_exists('viewPath')) {
+if (!function_exists('viewPath')) {
   function viewPath($view)
   {
     return __DIR__ . "/../../views/$view.php";
   }
 }
 
-if (!functon_exists('viewPath')) {
+if (!function_exists('viewPath')) {
   function viewJSON($data)
   {
     header('Content-Type: application/json;charset=utf-8');
